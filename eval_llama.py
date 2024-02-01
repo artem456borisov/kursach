78a79b1158ef77e17052a3f1188863aeda04befd
Artem <artemborisov17@gmail.com>
2024-02-01 18:02:43 +0300

    Added memory measurements, broken deepspeed implementation.

--- eval_llama.py
+++ eval_llama.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 import time
 import pandas as pd
 import numpy as np
+import deepspeed
 
 
 import yaml
@@ -48,6 +49,10 @@ if __name__ == "__main__":
     df['train'] = df['train'].to_pandas()[0:50]
     
     logger.info('loading the model and tokenizer')
+    #measuring memory usage before loading model
+    memory_initial = torch.cuda.memory_allocated(device)
+    logger.info(f"The initial allocated memory: {memory_initial}")
+    
     tokenizer = AutoTokenizer.from_pretrained(config["tokenizer"]["name_or_path"], 
                                               **config["tokenizer"]["kwargs"])
     tokenizer.padding_side = "left"
@@ -57,23 +62,51 @@ if __name__ == "__main__":
         **config["model"],
         torch_dtype=torch.bfloat16,
     )
+    
+    #use deepspeed if we want to
+    if config["inference_speedup"]["deepspeed"]:
+        model = deepspeed.init_inference(
+                    model=model,
+                    mp_size=config["inference_speedup"]["num_gpus"],  # Number of GPU.
+                    dtype=torch.float16,
+                    replace_method='auto',
+                    replace_with_kernel_inject=True
+                )
+
     model.eval()
+    
+    #Lets output the memory consumed by the model
+    memory_model = torch.cuda.memory_allocated(device)
+    logger.info(f"Model memory consumption: {memory_initial}")
     
     data = MERA_dataset(df['train'], tokenizer, config["tokenizer"]["kwargs"])
     full_dataloader = torch.utils.data.DataLoader(data, **config['loader_args'])
     
     preds = []
+    memory_consumed = []
     start = time.time()
     logger.info('starting evaluation')
     
+    
+    MAX_NEW_TOKENS = config['generation_args']['max_new_tokens']
+    MAX_LENGTH = config['tokenizer']['kwargs']['max_length']
     with torch.no_grad():
         for batch in tqdm(full_dataloader):
-            outputs = model.generate(**batch, max_new_tokens=config['generation_args']['max_new_tokens'])
+            prior_mem = torch.cuda.memory_allocated(device)
+            outputs = model.generate(**batch, max_new_tokens=MAX_NEW_TOKENS)
+            outputs = outputs[:, MAX_LENGTH:MAX_LENGTH+MAX_NEW_TOKENS]
+            # outputs = tokenizer.batch_decode(outputs)
+            # outputs = [i[-1] for i in outputs]
+            #only decode new tokens
             outputs = tokenizer.batch_decode(outputs)
-            outputs = [i[-1] for i in outputs]
+            post_mem = torch.cuda.memory_allocated(device)
             preds.extend(outputs)
+            memory_consumed.append(post_mem-prior_mem)
     end=time.time()    
-    logger.info('finished evaluation')
-    logger.info(f"inference time is: {end - start} seconds")
-    logger.info(f"the accuracy is: {(np.array(preds) == df['train']['outputs']).sum()/len(preds)}")
-    print('DONE')
+    logger.info('Finished evaluation')
+    logger.info(f"Inference time is: {end - start} seconds")
+    logger.info(f"The accuracy is: {(np.array(preds) == df['train']['outputs']).sum()/len(preds)}")
+    logger.info(f'''Average memory consumtion during forward pass, for batch size 
+                {config["loader_args"]["batch_size"]}: {np.array(memory_consumed).mean()}''')
+    logger.info(f"The current memory allocated at GPU: {torch.cuda.memory_allocated(device)}")
+    logger.info('DONE')
